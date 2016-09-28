819d794b9de9a6515d99090be9765714eaa840e1
Dan Linenberg <dan.linenberg@appsflyer.com>
2016-09-28 17:35:08 +0300

    SDK 4.5.5, updated ApiDefinition, added universal links in SampleApp

--- AppsFlyerSampleApp/AppsFlyerConversionDataDelegate.cs
+++ AppsFlyerSampleApp/AppsFlyerConversionDataDelegate.cs
@@ -7,7 +7,7 @@ namespace AppsFlyerSampleApp
 	public class AppsFlyerConversionDataDelegate : AppsFlyerTrackerDelegate
 	{
 		public override void OnAppOpenAttribution (NSDictionary attributionData) {
-
+			Console.WriteLine ("deeplink data in xamarin = " + attributionData.Description);
 		}
 
 		public override void OnAppOpenAttributionFailure (NSError error) {
