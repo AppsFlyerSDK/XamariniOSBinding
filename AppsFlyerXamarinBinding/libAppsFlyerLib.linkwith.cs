4d4c38c0e244d85dd51ede4b3b297e9fae7ad77e
Maksim Evtukh <maksevtukh@gmail.com>
2016-12-06 13:19:17 +0300

    Added native reference to libAppsFliyerLib.a

--- AppsFlyerXamarinBinding/libAppsFlyerLib.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using ObjCRuntime;
-
-[assembly: LinkWith ("libAppsFlyerLib.a", SmartLink = true, ForceLoad = true)]
-[assembly: LinkWith ("libAppsFlyerLib.a", SmartLink = true, Frameworks="Security")]
