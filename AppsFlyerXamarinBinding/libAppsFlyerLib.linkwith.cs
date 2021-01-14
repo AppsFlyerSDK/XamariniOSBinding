359380609e8ece7eef77cd44a54d2320cb62455d
af-vs <vitaly.sokolov@appsflyer.com>
2021-01-14 16:36:54 +0200

    Revert LinkWith params

--- AppsFlyerXamarinBinding/libAppsFlyerLib.linkwith.cs
+++ AppsFlyerXamarinBinding/libAppsFlyerLib.linkwith.cs
@@ -7,4 +7,4 @@
 
 using ObjCRuntime;
 
-[assembly: LinkWith ("libAppsFlyerLib.a", Frameworks = "Security", SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libAppsFlyerLib.a", SmartLink = true, Frameworks = "Security iAd", WeakFrameworks = "AdSupport")]
