5d33b49f2d2c899653ecaa365236d21d9bd4fb1e
Amit kremer <amit.appsflyer@appsflyer.com>
2024-02-26 12:48:31 +0200

    6.13.0 + dma

--- pack.sh
+++ pack.sh
@@ -1,8 +1,8 @@
 rm -rf AppsFlyerBinding.iOS/bin
 rm -rf AppsFlyerBinding.iOS/obj
 
-dotnet clean
-dotnet restore
+dotnet clean AppsFlyerBinding.iOS/
+dotnet restore AppsFlyerBinding.iOS/
 dotnet pack -c Release AppsFlyerBinding.iOS/AppsFlyerBinding.iOS.csproj
 
 mv AppsFlyerBinding.iOS/bin/Release/AppsFlyerXamarinBinding*.nupkg nugets/
